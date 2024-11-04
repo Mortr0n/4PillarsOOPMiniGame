e9dd34eed570bf84db35a055666b96d5c2802433
Mortron <cmsl4y3r@gmail.com>
2024-11-03 17:08:36 -0700

    added enemy base script for enemies to inherit from

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private float _moveSpeed = 10f;
+    public float MoveSpeed
     {
-        
+        get { return _moveSpeed; }
+        //TODO: think through the setter and getter here and what could I do to control these!
+        set  
+        {
+            _moveSpeed = value;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //TODO: make the same movement here as the MoveDown in my personal project maybe.  Make them so that they can be overridden so virtual or I think we can do abstract if we 
+    //TODO: want every object to set their own up.  Maybe actually make the weapons be abstract and each sets their own weapons.  so virtual for move so that I can adjust for 
+    //TODO: more asteroid like behavior or differing enemies and then abstract for weapons.
 }
